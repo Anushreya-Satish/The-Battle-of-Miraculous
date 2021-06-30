eb57852d4483f8fb7b1269b5412b9392ad61f131
Anushreya Satish <75848533+Anushreya-Satish@users.noreply.github.com>
2021-06-30 17:21:14 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -8,7 +8,7 @@ var plagg, plaggImg;
 var tikki, tikkiImg;
 var key, keyImg;
 var jail, jailImg;
-var spike1, spike2, spike3, spike4, spikeImg;
+var spike1, spike2, spike3, spikeImg;
 var ball1, ball2, ballImg;
 var plate, plateImg;
 var adrien, adrienImg, catnoirImg;
@@ -28,6 +28,8 @@ var more, moreImg;
 var info, infoImg;
 var game1, game2, game3, game1Img, game2Img, game3Img;
 var music1, music2, music3;
+var edges, death, touch;
+var 
 
 function preload() {
   //load backgrounds
@@ -112,10 +114,10 @@ function setup() {
   wall1 = createSprite(340, 150, 50, 10);
   wall1.addImage(wallImg);
   wall1.scale = 1.2;
-  wall2 = createSprite(1130, 350, 10, 10);
+  wall2 = createSprite(1130, 370, 10, 10);
   wall2.addImage(wallImg);
   wall2.scale = 1.2;
-  wall3 = createSprite(340, 550, 10, 10);
+  wall3 = createSprite(340, 570, 10, 10);
   wall3.addImage(wallImg);
   wall3.scale = 1.2;
   wall1.visible = false;
@@ -128,7 +130,7 @@ function setup() {
   tikki.visible = false;
 
   //create player Plagg
-  plagg = createSprite(50, 650, 10, 10);
+  plagg = createSprite(50, 670, 10, 10);
   plagg.addImage(plaggImg);
   plagg.visible = false;
 
@@ -138,9 +140,10 @@ function setup() {
   key.visible = false;
 
   //create level 1 plate
-  plate = createSprite(1400, 600, 10, 10);
+  plate = createSprite(1400, 570, 10, 10);
   plate.addImage(plateImg);
   plate.scale = 0.8;
+  plate.velocityX = -3;
   plate.visible = false;
 
   //create level 1 balls
@@ -148,22 +151,45 @@ function setup() {
   ball1.addImage(ballImg);
   ball2 = createSprite(200, 450, 10, 10);
   ball2.addImage(ballImg);
+  ball1.velocityY = 4;
+  ball2.velocityY = -4;
   ball1.visible = false;
   ball2.visible = false;
 
   //create level 2 spikes
-  spike1 = createSprite(1200, 200, 10, 10);
+  spike1 = createSprite(1300, 200, 10, 10);
   spike1.addImage(spikeImg);
-  spike2 = createSprite(1300, 200, 10, 10);
+  spike2 = createSprite(900, 200, 10, 10);
   spike2.addImage(spikeImg);
-  spike3 = createSprite(1400, 200, 10, 10);
+  spike3 = createSprite(1100, 200, 10, 10);
   spike3.addImage(spikeImg);
-  spike4 = createSprite(1500, 200, 10, 10);
-  spike4.addImage(spikeImg);
+  spike1.velocityY = -4;
+  spike2.velocityY = -4;
+  spike3.velocityY = -4;
   spike1.visible = false;
   spike2.visible = false;
   spike3.visible = false;
-  spike4.visible = false;
+
+  //create adrien
+  adrien = createSprite(1300,500,10,10);
+  adrien.addImage(adrienImg);
+  adrien.scale = 1;
+  adrien.visible = false;
+
+  //create marinette
+  marinette = createSprite(1400,500,10,10);
+  marinette.addImage(marinetteImg);
+  marinette.scale = 0.6;
+  marinette.visible = false;
+
+  //create edge sprites
+  edges = createEdgeSprites();
+
+  //touch
+  touch = false;
+
+  //store value of death
+  death = 0;
 
   //Game State value initially is 0
   gameState = 0;
@@ -207,20 +233,105 @@ function draw() {
     spike1.visible = true;
     spike2.visible = true;
     spike3.visible = true;
-    spike4.visible = true;
+
+    stroke("black");
+    strokeWeight(5);
+    textSize(30);
+    fill("red");
+    textFont("Forte")
+    text("Death:"+ death, 1400,50);
 
     if (keyDown(RIGHT_ARROW)) {
-      plagg.x = plagg.x + 4;
+      plagg.x = plagg.x + 5;
     }
     if (keyDown(LEFT_ARROW)) {
-      plagg.x = plagg.x - 4;
+      plagg.x = plagg.x - 5;
     }
     if (keyDown(UP_ARROW)) {
-      plagg.y = plagg.y - 4;
+      plagg.y = plagg.y - 5;
     }
     if (keyDown(DOWN_ARROW)) {
-      plagg.y = plagg.y + 4;
+      plagg.y = plagg.y + 5;
     }
+
+      ball1.bounceOff(wall1);
+      ball1.bounceOff(wall3);
+      ball2.bounceOff(wall1);
+      ball2.bounceOff(wall3);
+      plate.bounceOff(wall3);
+      plate.bounceOff(edges[1]);
+      spike1.bounceOff(wall2);
+      spike2.bounceOff(wall2);
+      spike3.bounceOff(wall2);
+      spike1.bounceOff(edges[2]);
+      spike2.bounceOff(edges[2]);
+      spike3.bounceOff(edges[2]);
+
+      /*if(plagg.isTouching(wall1)||plagg.isTouching(wall2)||plagg.isTouching(wall3)){
+        plagg.x = 50;
+        plagg.y = 670;
+        death = death + 1;
+      }
+      if(plagg.isTouching(spike1)||plagg.isTouching(spike2)||plagg.isTouching(spike3)){
+        plagg.x = 50;
+        plagg.y = 670;
+        death = death + 1
+      }
+      if(plagg.isTouching(ball1)||plagg.isTouching(ball2)||plagg.isTouching(plate)){
+        plagg.x = 50;
+        plagg.y = 670;
+        death = death + 1;
+      }*/
+      if(plagg.isTouching(tikki)){
+        gameState = 4;
+      }
+      /*if(plagg.isTouching(key)){
+        touch = true;
+      }
+      if(touch === true){
+        jail.destroy();
+      }
+      if(touch === false && plagg.isTouching(jail)){
+        if(keyDown(LEFT_ARROW)){
+          plagg.x = plagg.x;
+        }
+      }*/
+  }
+  if(gameState === 4){
+    background(bg5);
+    jail.visible = false;
+    next.visible = true;
+    wall1.visible = false;
+    wall2.visible = false;
+    wall3.visible = false;
+    tikki.visible = false;
+    plagg.visible = false;
+    key.visible = false;
+    plate.visible = false;
+    ball2.visible = false;
+    ball1.visible = false;
+    spike1.visible = false;
+    spike2.visible = false;
+    spike3.visible = false;
+    if (mousePressedOver(next)) {
+      gameState = 5;
+    }
+  }
+  if(gameState === 5){
+    background(bg6);
+    plagg.visible = true;
+    adrien.visible = true;
+    tikki.visible = true;
+    marinette.visible = true;
+    next.visible = false;
+
+    plagg.x = 1300;
+    tikki.x = 1400;
+    plagg.y = 200;
+    tikki.y = 200;
+    plagg.scale = 0.5;
+    tikki.scale = 0.5;
+
   }
 
   drawSprites();
